49d6a1604ff973a511e585a8234a424763db003a
liuyixi <liuyixi@xiaomi.com>
2019-02-22 13:49:40 +0800

    sofia:add feature for remove data.

--- DataBase/app/src/main/java/databasepractice/sofia/com/database/MainActivity.java
+++ DataBase/app/src/main/java/databasepractice/sofia/com/database/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private SofiaDatabaseHelper dbHelper;
     private Button mAddData;
     private Button mUpdateData;
+    private Button mRemoveData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //版本号可以让onUpdate方法执行起来
         //数据库的名字
-        dbHelper = new SofiaDatabaseHelper(this,"BookStore.db",null,3);
+        dbHelper = new SofiaDatabaseHelper(this,"BookStore.db",null,4);
         Button createDatabase = (Button)findViewById(R.id.create_database);
         createDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
                 values.put("price",11.11);
                 //更新哪个表中的哪个值，哪一列
                 db.update("Book",values,"name = ?",new String[]{"hahaji"});
+            }
+        });
+
+        //删除数据
+        mRemoveData = (Button)findViewById(R.id.remove_data);
+        mRemoveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book","pages > ?" ,new String[]{"805"});
             }
         });
     }
