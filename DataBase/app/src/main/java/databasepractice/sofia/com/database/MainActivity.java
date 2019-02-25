74d851b75839a6af33a0c856b4df46803443ca84
liuyixi <liuyixi@xiaomi.com>
2019-02-25 18:01:54 +0800

    sofia:update query data feature.

--- DataBase/app/src/main/java/databasepractice/sofia/com/database/MainActivity.java
+++ DataBase/app/src/main/java/databasepractice/sofia/com/database/MainActivity.java
@@ -1,8 +1,10 @@
 package databasepractice.sofia.com.database;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mAddData;
     private Button mUpdateData;
     private Button mRemoveData;
+    private Button mQueryData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +74,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 db.delete("Book","pages > ?" ,new String[]{"805"});
+            }
+        });
+
+        //查询数据
+        mQueryData = (Button)findViewById(R.id.query_data);
+        mQueryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //查询Book中所有的数据
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if(cursor.moveToFirst()){
+                    do{
+                        //遍历Cursor对象取出并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        Double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        int page = cursor.getInt(cursor.getColumnIndex("pages"));
+                        Log.d("liuyixi222","name is = " + name + " ;author = " + author + " ; price = " + price + " ; page = " + page);
+                    }while(cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
